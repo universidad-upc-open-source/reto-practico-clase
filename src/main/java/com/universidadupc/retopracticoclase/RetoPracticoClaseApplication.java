d1aeaf5efe60621b257e04ba624c0c84be405b56
Juan Antonio <juan.cahuana@globant.com>
2022-10-25 22:14:22 -0500

    First commit

---
+++ src/main/java/com/universidadupc/retopracticoclase/RetoPracticoClaseApplication.java
@@ -0,0 +1,13 @@
+package com.universidadupc.retopracticoclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoPracticoClaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoPracticoClaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/universidadupc/retopracticoclase/RetoPracticoClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidadupc.retopracticoclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoPracticoClaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
